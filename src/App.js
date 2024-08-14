ff06ab65edefaa7f5da8c2fc234cbf0bdaf99db3
Ariuna <dasadondokovaariuna@gmail.com>
2024-08-13 20:14:41 -0400

    modified:

--- src/App.js
+++ src/App.js
@@ -9,32 +9,9 @@ import PlaceList from './PlaceList';
 import "./App.css";
 
 function App() {
-  const [restaurant, setRestaurant] = useState(0);
   const [gifts, setGifts] = useState(datatwo);
   const [places, setPlaces] = useState(datathree);
   const [showText, setShowText] = useState(false);
-
-  const { id, name, description, address, image } = data[restaurant];
-
-  const previousRestaurant = () => {
-    setRestaurant((restaurant) => {
-      restaurant--;
-      if (restaurant < 0) {
-        return data.length - 1;
-      }
-      return restaurant;
-    });
-  };
-
-  const nextRestaurant = () => {
-    setRestaurant((restaurant) => {
-      restaurant++;
-      if (restaurant > data.length - 1) {
-        restaurant = 0;
-      }
-      return restaurant;
-    });
-  };
 
   const removeGift = (idtwo) => {
     let newGifts = gifts.filter((gift) => gift.idtwo !== idtwo);
@@ -54,15 +31,7 @@ function App() {
   return (
     <div>
       <Header />
-      <Restaurant
-        id={id}
-        name={name}
-        description={description}
-        address={address}
-        image={image}
-        previousRestaurant={previousRestaurant}
-        nextRestaurant={nextRestaurant}
-      />
+      <Restaurant data={data} /> 
       <GiftList gifts={gifts} removeGift={removeGift} setGifts={setGifts} />
       <PlaceList places={places} removePlace={removePlace} showTextClick={showTextClick} />
     </div>
